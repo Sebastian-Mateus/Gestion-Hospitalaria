e03b06dd6654645a104ee966de143b4e996d82e1
Sebastian-Mateus <juans.mateush@uqvirtual.edu.co>
2025-03-18 07:39:07 -0500

    Modulo Medico

---
+++ src/main/java/co/edu/uniquindio/poo/gestionhospitalaria/controller/MedicalRecordServiceController.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.poo.gestionhospitalaria.controller;
+
+import co.edu.uniquindio.poo.gestionhospitalaria.model.Hospital;
+import co.edu.uniquindio.poo.gestionhospitalaria.model.MedicalRecord;
+
+public class MedicalRecordService {
+
+    private Hospital hospital;
+    private MedicalRecord medicalRecord;
+}
---
+++ src/main/java/co/edu/uniquindio/poo/gestionhospitalaria/controller/MedicationDiagnosisServiceController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.poo.gestionhospitalaria.controller;
+
+public class MedicationDiagnosisServiceController {
+}
